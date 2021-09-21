c5eb5dcde2203c83658113fe5cab3c18c38fb6f7
root <root@kloud-server-103810.kloud.co.uk>
2021-09-21 10:34:36 +0000

    Add custom config

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict';
 const express = require('express');
+const path = require('path');
+
+const config = require(path.resolve('..', , 'conf', `${path.dirname(__dirname)}.config.js`));
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 express()
-  .get('/', (req, res) => res.send('Hello World'))
+  .get('/', (req, res) => res.send('Hello World\n\n${JSON.stringify(config, null, 2)}'))
   .listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
