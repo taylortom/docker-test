79e69d3078d86bd521479b61bfd0536181173a77
Tom Taylor <hello@tomtaylor.name>
2021-09-21 11:51:11 +0100

    Fix

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const path = require('path');
 
-const config = require(path.resolve('..', , 'conf', `${path.dirname(__dirname)}.config.js`));
+const config = require(path.resolve(`../conf/${path.basename(__dirname)}.config.js`));
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 express()
-  .get('/', (req, res) => res.send('Hello World\n\n${JSON.stringify(config, null, 2)}'))
+  .get('/', (req, res) => res.send(`Hello World\n\n${JSON.stringify(config, null, 2)}`))
   .listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
